bdee84867de89578c1f1ba0e653507760f7e46ca
Vipul <vipul.sharma20@gmail.com>
2017-12-23 13:13:45 +0530

    update pagination value

--- bikochu/settings.py
+++ bikochu/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 10,
 }
 
 # Application definition
